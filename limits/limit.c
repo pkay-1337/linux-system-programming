7d781d489eed76d56d1d6ba03defe2a6fd709ae1
Pramod <kumarpramod66476@gmail.com>
2024-11-04 10:49:40 +0530

    add limits section

---
+++ limits/limit.c
@@ -0,0 +1,18 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Working\n");
+  long m = sysconf(_SC_OPEN_MAX);
+  printf("Max fd : %ld\n", m);
+  printf("Page size : %ld\n", sysconf(_SC_PAGESIZE));
+  printf("File name : %ld\n", pathconf("./",_PC_NAME_MAX));
+  return EXIT_SUCCESS;
+}
+
+
+
