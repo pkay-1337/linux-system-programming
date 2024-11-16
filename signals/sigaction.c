79b01bc1cc438b31dce4a439072e2ee759e8c39e
Pramod <kumarpramod66476@gmail.com>
2024-11-16 09:30:09 +0530

    altstack for signal handler using sigaction

--- signals/sigaction.c
+++ signals/sigaction.c
@@ -2,18 +2,38 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <sys/mman.h>
 
 void handler(int s){
+  int a = 10;
+  printf("i am at %p in handler\n", &a);
   printf("clicked CTRL+c\n");
 }
 
 int main(){
+  
+  void *mapped = mmap((void *)0x13370000, 0x10000, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+  printf("Mapped memory here : %p\n", mapped);
+
+  stack_t *sigstack = malloc(sizeof(stack_t));
+  sigstack->ss_sp = (void *)0x13370000;
+  sigstack -> ss_flags = SS_ONSTACK;
+  sigstack -> ss_size = 0x10000;
+  assert(sigaltstack(sigstack, NULL) == 0);
+
+  printf("alt stack start here : %p\n", sigstack->ss_sp);
+
+
   sigset_t *set = malloc(sizeof(sigset_t));
   sigaddset(set, SIGTERM);
   struct sigaction *act = malloc(sizeof(struct sigaction));
   act -> sa_handler = handler;
   act -> sa_mask = *set;
+  act -> sa_flags = SA_ONSTACK;
   sigaction(2, act, NULL);
+  int a = 10;
+  printf("i am at %p in main\n", &a);
   pause(); 
   printf("back in main\n");
   pause();
