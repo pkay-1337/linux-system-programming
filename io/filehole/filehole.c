7e0aefe2f31ec1cbc99b6f1e8adaee925f766a60
Pramod <kumarpramod66476@gmail.com>
2024-11-16 13:40:00 +0530

    run it and see magic

--- io/filehole/filehole.c
+++ io/filehole/filehole.c
@@ -1,8 +1,9 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 int main(){
-  int fd = open("test", O_RDWR);
-  lseek(fd, 10000, SEEK_END);
+  int fd = open("file", O_RDWR | O_CREAT);
+  lseek(fd, 536870912000, SEEK_END);
   char * buff = "AAAAAAAAAAAAAA";
   write(fd, buff, sizeof(buff));
   close(fd);
