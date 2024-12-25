c40a3d0c2c1786d4e2ad7dc5da625532c77c867b
Pramod <kumarpramod66476@gmail.com>
2024-12-25 16:14:13 +0530

    better sender

--- signals/realtimesigsender.c
+++ signals/realtimesigsender.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
 #include <signal.h>
+#include <stdio.h>
 #include <unistd.h>
 
-void main(){
+int main() {
   int pid, sig;
   union sigval u;
   printf("MY PID : %d\n", getpid());
   printf("Enter PID : ");
   scanf("%d", &pid);
-  printf("Enter SIGNAL NUMBER : ");
-  scanf("%d", &sig);
-  while(1){
+  while (1) {
+    printf("Enter SIGNAL NUMBER : ");
+    scanf("%d", &sig);
     printf("Enter Integer data : ");
     scanf("%d", &u.sival_int);
     sigqueue(pid, sig, u);
   }
+  return 0;
 }
