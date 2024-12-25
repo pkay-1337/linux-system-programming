0d5dbfb7048ad99f67a7a40f277163c193095e57
Pramod <kumarpramod66476@gmail.com>
2024-12-25 16:14:00 +0530

    better receiver

--- signals/realtimesigreciever.c
+++ signals/realtimesigreciever.c
@@ -1,21 +1,23 @@
 #include <signal.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-void handler(int s, siginfo_t *i, void *x){
+void handler(int s, siginfo_t *i, void *x) {
   printf("signal : %d\n", s);
-  printf("sender : %d\n", i -> si_pid);
-  printf("value : %d\n", i -> si_value.sival_int);
+  printf("sender : %d\n", i->si_pid);
+  printf("value : %d\n", i->si_value.sival_int);
 }
-int main(){
+int main() {
   printf("MY PID : %d\n", getpid());
   printf("%d - %d\n", SIGRTMIN, SIGRTMAX);
   struct sigaction *act = malloc(sizeof(struct sigaction));
-  act -> sa_sigaction = handler;
-  act -> sa_flags = SA_SIGINFO;
-  sigaction(34, act, NULL);
-  while (1){
+  act->sa_sigaction = handler;
+  act->sa_flags = SA_SIGINFO;
+  for (int i = 34; i < 65; i++) {
+    sigaction(i, act, NULL);
+  }
+  while (1) {
     pause();
     printf("------------\n");
   }
