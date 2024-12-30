0eb5127989bcda1d92f51a7faa23258f1be858c4
Pramod <kumarpramod66476@gmail.com>
2024-12-30 10:42:30 +0530

    wait what?

---
+++ process/what.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  printf("Hello, World!\n");
+  write(1, "ciao\n", 5);
+  fork();
+  /* _exit(69); */
+  exit(69);
+}
+/* Cooooooooooooooooooool */
