ecad73ff0d547ffa3f899ac818166293970724f4
Pramod <kumarpramod66476@gmail.com>
2024-12-30 10:42:04 +0530

    exit

--- process/exit.c
+++ process/exit.c
@@ -6,10 +6,13 @@ void cleaner() {
   printf("Bye, World!\n");
   /* exit(69); */
 }
+void cleaner2(int a, void *s) { printf("%d %s\n", a, (char *)s); }
 
 int main(int argc, char *argv[]) {
   atexit(cleaner);
+  on_exit(cleaner2, "Message");
   printf("Hello, World!\n");
   /* _exit(99); */
+  exit(69);
   return EXIT_SUCCESS;
 }
