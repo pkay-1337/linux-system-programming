6ea83d303ecddd4987caee04346bacda300e0eb5
pkay <kumarpramod66476@gmail.com>
2025-07-08 15:38:23 +0530

    cleaned

--- systemInfo/listproc.c
+++ systemInfo/listproc.c
@@ -10,30 +10,76 @@
 
 char user[255];
 char path[4096];
-int fd;
-char statusline[1024];
-char *pstr = "Pid:\t";
-char *nstr = "Name:\t";
-char pid[10];
-char name[256];
+// char statusline[1024];
+// char pid[20];
+// char name[256];
 char temp[1] = {1};
 int l = 1;
 char uuid[20];
-char uid[20];
-int main(int argc, char *argv[]){
+// char uid[20];
+
+char * getArg(int argc, char *argv[]){
   char *optstring = ":u:";
   int opt = getopt(argc, argv, optstring);
   if((unsigned char) opt == 'u'){
-    sprintf(user, "%s", optarg);
-    printf("user : %s\n", user);
-    
-    struct passwd *pwd;
-    pwd = getpwnam(user);
-    if(pwd != NULL) {
-      printf("UID : %d\n", pwd -> pw_uid);
-      sprintf(uuid, "%d", pwd -> pw_uid);
-      // printf("UID str : %s\n", uid);
+    return optarg;
+  }else if((unsigned char) opt == ':'){
+    printf("Argument missing : -%c <user>\n", (unsigned char)optopt);
+    exit(1);
+  }else{
+    printf("Argument missing : -u <user>\n");
+    exit(1);
+  }
+}
+
+int getUUID(char* user){
+  struct passwd *pwd;
+  pwd = getpwnam(user);
+  if(pwd != NULL) {
+    return pwd -> pw_uid;
+  }
+}
+
+struct details {
+  char pid[20];
+  char name[256];
+  char uid[20];
+};
+
+struct details* getDetails(char * path){
+  struct details *d;
+  char statusline[1024] = {0};
+  d = (struct details *) malloc(sizeof(struct details));
+  int fd = open(path, O_RDONLY);
+  if (fd != -1){
+    l = 1;
+    while (l != 0){
+      for(int i = 0;;i++){
+        l = read(fd, temp, 1);
+        if(temp[0] == EOF || temp[0] == '\n') break;
+        statusline[i] = temp[0];
+      }
+      if(strncmp("Uid:\t", statusline, 5) == 0){
+        sscanf(statusline, "Uid:\t%s\t", d->uid);
+      }
+      if(strncmp("Pid:\t", statusline, 5) == 0){
+        sscanf(statusline, "Pid:\t%s\t", d->pid);
+      }
+      if(strncmp("Name:\t", statusline, 6) == 0){
+        sscanf(statusline, "Name:\t%s\n", d->name);
+      }
+      memset(statusline, 0, 1024);
     }
+    close(fd);
+  }
+  return d;
+}
+
+
+int main(int argc, char *argv[]){
+    char *user = getArg(argc, argv);
+    printf("user : %s\n", user);
+    sprintf(uuid, "%d", getUUID(user));
 
     DIR *dir;
     dir = opendir("/proc/");
@@ -45,49 +91,12 @@ int main(int argc, char *argv[]){
     while( d = readdir(dir)){
       if( d -> d_name[0] >= 0x30 && d -> d_name[0] <= 0x39 ){
         sprintf(path, "/proc/%s/status", d -> d_name);
-        fd = open(path, O_RDONLY);
-        if (fd != -1){
-          l = 1;
-          while (l != 0){
-            for(int i = 0;;i++){
-              l = read(fd, temp, 1);
-              if(temp[0] == EOF || temp[0] == '\n') break;
-              statusline[i] = temp[0];
-            }
-            if(strncmp("Uid:\t", statusline, 5) == 0){
-              sscanf(statusline, "Uid:\t%s\t", uid);
-            }
-            if(strncmp(pstr, statusline, 5) == 0){
-              sscanf(statusline, "Pid:\t%s\t", pid);
-            }
-            if(strncmp(nstr, statusline, 6) == 0){
-              sscanf(statusline, "Name:\t%s\n", name);
-            }
-            memset(statusline, 0, 1024);
-          }
-
-          if(strcmp(uid, uuid) == 0)
-          printf("%s\t---\t%s\n", pid, name);
-          // printf("Owner uid : %s\n", uid);
-          memset(pid, 0, 10);
-          memset(name, 0, 256);
-        }
-        close(fd);
+        struct details *d;
+        d = getDetails(path);
+        if(strcmp(d->uid, uuid) == 0)
+        printf("%s\t---\t%s\n", d->pid, d->name);
+        free(d);
       }
     }
-
-
-    
-
-
-
-
-
-
-  }else if((unsigned char) opt == ':'){
-    printf("Argument missing : -%c <user>\n", (unsigned char)optopt);
-  }else{
-    printf("Argument missing : -u <user>\n");
-  }
   return EXIT_SUCCESS;
 }
